4de783e2bcee7f35c2dc408c698a3a1b467ebc00
kandula1578 <88316080+kandula1578@users.noreply.github.com>
2023-04-05 17:39:20 +0530

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-console.log("test");
+console.log("testing");
 
 app.get('/', function (req, res) {
     res.send('{ "response": "Hello, Welcome to Valaxy" }');
